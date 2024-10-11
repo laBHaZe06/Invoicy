c50a51436f05b0b5b04ce6cf48b4f0694bf30d30
laBHaZe06 <borne.yoan@outlook.fr>
2024-10-11 10:54:31 +0200

    update

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -16,6 +16,11 @@ export default function AdminPage() {
         backgroundColor: '#f5f5f5',
         height: '100vh',
         width: '100vw',
+        justifyContent: 'center',
+        color: '#333',
+        fontSize: '24px',
+        fontWeight: 'bold',
+        fontFamily: 'Geist Sans',
       }}
     >
       <Typography>Hello</Typography>
