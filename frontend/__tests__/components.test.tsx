94634b8ac3443e567de2936ce2ff3103f2d0efb6
laBHaZe06 <borne.yoan@outlook.fr>
2024-10-11 16:56:48 +0200

    create folder component + first component nav bar

--- frontend/__tests__/components.test.tsx
+++ frontend/__tests__/components.test.tsx
@@ -2,16 +2,14 @@
 import { render, screen } from '@testing-library/react'
 import {describe, expect} from '@jest/globals';
 
-import HomePage from '../src/app/home/page';
 
 
-describe("Home", () => {
+describe("Components", () => {
   it("renders a heading", () => {
-    render(<HomePage />);
+    render('');
 
     const heading = screen.getByRole("heading", {level: 1});
+
     expect(heading).toBeDefined();
   });
 });
-
-
