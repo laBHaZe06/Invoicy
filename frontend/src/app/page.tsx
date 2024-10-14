c4b422ca1810db15f7719f16332847b308b5a230
laBHaZe06 <borne.yoan@outlook.fr>
2024-10-14 10:53:02 +0200

    update navbar + creation hero (page accueil)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,16 @@
 import NavBarComponent from "./components/NavBarComponent";
+import FooterComponent from "./components/FooterComponent";
+import Box from '@mui/material/Box';
+import Hero from "./components/Hero";
+
 export default function Page() {
   return (
+    <>
     <NavBarComponent/>
+    <Box sx={{mt: 5}}>
+      <Hero/>
+    <FooterComponent/>
+    </Box>
+    </>
   );
 }
