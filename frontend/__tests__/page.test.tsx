26995353f282663be74a51b93f25f70ec7cfdac7
laBHaZe06 <borne.yoan@outlook.fr>
2025-03-05 14:11:53 +0100

    register and activation by email

--- frontend/__tests__/page.test.tsx
+++ frontend/__tests__/page.test.tsx
@@ -1,10 +1,8 @@
 
 import { render, screen } from '@testing-library/react'
 import {describe, expect} from '@jest/globals';
-
-import HomePage from '../src/app/home/page';
 import Page from '@/app/page';
-import ConnectComponent from '@/app/components/ConnectComponent';
+
 
 // Test for Page App component
 describe("Page App", () => {
@@ -15,28 +13,6 @@ describe("Page App", () => {
     // const buttonStart = screen.getByRole('button', { name: 'start' });
     expect(heading).toBeDefined();
     // expect(buttonStart).toBeDefined();
-  });
-});
-
-
-// Test for Home Page component
-describe("Home Page", () => {
-  it("renders a heading", () => {
-    render(<HomePage />);
-
-    const heading = screen.getByRole("heading", {level: 1});
-    expect(heading).toBeDefined();
-  });
-});
-
-
-// Test connection component
-describe("Connection Component", () => {
-  it("renders a heading", () => {
-    render(<ConnectComponent />);
-
-    const heading = screen.getByText(/se connecter/i);
-    expect(heading).toBeDefined();
   });
 });
 
